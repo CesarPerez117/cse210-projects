1663758c82ff426365e0721b073d3e41ca5630e2
CesarPerez117 <cesarperezp117@gmail.com>
2023-01-19 21:52:10 -0500

    .

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Security Supervisor";
+        job1._company = "City Bank";
+        job1._startYear = 2005;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Security Agent";
+        job2._company = "Back Of America";
+        job2._startYear = 2002;
+        job2._endYear = 2005;
+
+        Resume myResume = new Resume();
+        myResume._name = "George Rojas";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
