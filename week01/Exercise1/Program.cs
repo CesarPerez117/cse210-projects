781e4202e873f15282fe05c1608f5361ba1752c5
cesarperez117 <cperezpineda@byupathway.edu>
2026-01-10 20:56:12 -0400

    first exercise completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        /*
+        Prompt the user for their first name. Then, prompt them for their last name. Display the text back 
+        all on one line saying, "Your name is last-name, first-name, last-name" as shown:
+
+        What is your first name? Scott
+        What is your last name? Burton
+
+        Your name is Burton, Scott Burton.
+         */
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
