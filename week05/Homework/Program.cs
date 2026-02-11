351938f8a3e97ea2f8bc36e383657e5b1e6cac5d
cesarperez117 <cperezpineda@byupathway.edu>
2026-02-11 19:13:21 -0400

    learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Miguel Gonzales","Factorization Cases");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "5.2", "22-23");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Mariana Luna", "Historia Colombiana", "Errores de la Democracia");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
